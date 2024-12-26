407e83eda0f19f493ef7187d4b8139a3de0de68e
Yehor Gordiienko <egordienko319@gmail.com>
2024-12-26 20:53:44 +0300

    test

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,24 @@
-export const PostInfo = () => <>Put the post here</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+import { CommentList } from '../CommentList/CommentList';
+
+export const PostInfo = ({ post }) => {
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{post.title}</h3>
+
+      <p>
+        {' Posted by  '}
+
+        <a className="UserInfo" href="mailto:Sincere@april.biz">
+          <UserInfo user={post.user} />
+        </a>
+      </p>
+    </div>
+
+    <p className="PostInfo__body">{post.body}</p>
+
+    <hr />
+
+    <CommentList comments={post.comment} />
+  </div>;
+};
